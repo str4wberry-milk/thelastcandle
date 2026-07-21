b33a4c23971885e04fa95c8750503452825ab5f8
squirtle <chrisli2014feb@gmail.com>
2026-07-21 17:01:31 +1000

    Implement dynamic light mode switching on button hover
    Added mouseenter and mouseleave event listeners to buttons for dynamic class changes.

--- game.js
+++ game.js
@@ -33,3 +33,48 @@ console.log(
 
 
 });
+
+const buttons = document.querySelectorAll("nav button");
+
+
+buttons[0].addEventListener("mouseenter",()=>{
+
+    document.body.classList.remove("light-dark");
+
+    document.body.classList.add("light-bright");
+
+});
+
+
+
+buttons[1].addEventListener("mouseenter",()=>{
+
+    document.body.classList.remove("light-dark");
+
+    document.body.classList.remove("light-bright");
+
+});
+
+
+
+buttons[2].addEventListener("mouseenter",()=>{
+
+    document.body.classList.remove("light-bright");
+
+    document.body.classList.add("light-dark");
+
+});
+
+
+
+buttons.forEach(button=>{
+
+    button.addEventListener("mouseleave",()=>{
+
+        document.body.classList.remove("light-bright");
+
+        document.body.classList.remove("light-dark");
+
+    });
+
+});
